3552cc862cbd7a9262b8682d9eee3f881ac555ff
repolliousa-commits <repolliousa@gmail.com>
2026-04-20 22:43:46 -0400

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,18 +1,14 @@
-const CACHE_NAME = 'invoice-gen-v1';
+const CACHE_NAME = 'invoice-v2';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js',
-  'https://unpkg.com/lucide@latest',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+  './manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
