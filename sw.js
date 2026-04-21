042c69d9d428c435ab7f7abc8960c4dd2541d4a7
repolliousa-commits <repolliousa@gmail.com>
2026-04-20 22:58:44 -0400

    Remove install and fetch event listeners from sw.js
    Remove service worker event listeners for install and fetch.

--- sw.js
+++ sw.js
@@ -1,9 +1,1 @@
-self.addEventListener('install', (e) => {
-  console.log('SW instalado');
-  self.skipWaiting();
-});
 
-self.addEventListener('fetch', (e) => {
-  // Pass-through para evitar bloqueos durante desarrollo
-  e.respondWith(fetch(e.request));
-});
